e45e0f31cda33381330de92634b8ab14e34010e3
Adrien Abbey <abbey.12@wright.edu>
2024-01-05 10:16:32 -0500

    correctly parsing input now

--- day5.java
+++ day5.java
@@ -5,11 +5,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 class day5 {
     /* Global Variables */
     public static String inputFileName = "example-input.txt";
+    public static boolean testing = true;
 
     public static void main(String[] args) throws FileNotFoundException {
         // Load the input into an array of strings:
@@ -17,8 +19,13 @@ class day5 {
 
         // Create array lists to hold relevant data:
         ArrayList<Integer> seeds = new ArrayList<>();
-        ArrayList<Integer[]> seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap,
-                lightToTemperatureMap, temperatureToHumidityMap, humidityToLocationMap = new ArrayList<>();
+        ArrayList<Integer[]> seedToSoilMap = null;
+        ArrayList<Integer[]> soilToFertilizerMap = null;
+        ArrayList<Integer[]> fertilizerToWaterMap = null;
+        ArrayList<Integer[]> waterToLightMap = null;
+        ArrayList<Integer[]> lightToTemperatureMap = null;
+        ArrayList<Integer[]> temperatureToHumidityMap = null;
+        ArrayList<Integer[]> humidityToLocationMap = null;
 
         // Parse the input into a usable format:
         for (int i = 0; i < inputStrings.size(); i++) {
@@ -28,6 +35,72 @@ class day5 {
             }
 
             // Check for "seed-to-soil":
+            if (inputStrings.get(i).contains("seed-to-soil")) {
+                seedToSoilMap = parseNumbers(inputStrings, i);
+            }
+
+            // Check for "soil-to-fertilizer":
+            if (inputStrings.get(i).contains("soil-to-fertilizer")) {
+                soilToFertilizerMap = parseNumbers(inputStrings, i);
+            }
+
+            // Check for "fertilizer-to-water":
+            if (inputStrings.get(i).contains("fertilizer-to-water")) {
+                fertilizerToWaterMap = parseNumbers(inputStrings, i);
+            }
+
+            // Check for "water-to-light":
+            if (inputStrings.get(i).contains("water-to-light")) {
+                waterToLightMap = parseNumbers(inputStrings, i);
+            }
+
+            // Check for "light-to-temperature":
+            if (inputStrings.get(i).contains("light-to-temperature")) {
+                lightToTemperatureMap = parseNumbers(inputStrings, i);
+            }
+
+            // Check for "temperature-to-humidity":
+            if (inputStrings.get(i).contains("temperature-to-humidity")) {
+                temperatureToHumidityMap = parseNumbers(inputStrings, i);
+            }
+
+            // Check for "humidity-to-location":
+            if (inputStrings.get(i).contains("humidity-to-location")) {
+                humidityToLocationMap = parseNumbers(inputStrings, i);
+            }
+        }
+
+        // Test code:
+        if (testing) {
+            System.out.println(" seeds: " + seeds.toString());
+            System.out.println(" seed-to-soil map: ");
+            for (Integer[] intArr : seedToSoilMap) {
+                System.out.println(" - " + Arrays.toString(intArr));
+            }
+            System.out.println(" soil-to-fertilizer map: ");
+            for (Integer[] intArr : soilToFertilizerMap) {
+                System.out.println(" - " + Arrays.toString(intArr));
+            }
+            System.out.println(" fertilizer-to-water map: ");
+            for (Integer[] intArr : fertilizerToWaterMap) {
+                System.out.println(" - " + Arrays.toString(intArr));
+            }
+            System.out.println(" water-to-light map: ");
+            for (Integer[] intArr : waterToLightMap) {
+                System.out.println(" - " + Arrays.toString(intArr));
+            }
+            System.out.println(" light-to-temperature map: ");
+            for (Integer[] intArr : lightToTemperatureMap) {
+                System.out.println(" - " + Arrays.toString(intArr));
+            }
+            System.out.println(" temperature-to-humidity map: ");
+            for (Integer[] intArr : temperatureToHumidityMap) {
+                System.out.println(" - " + Arrays.toString(intArr));
+            }
+            System.out.println(" humidity-to-location map: ");
+            for (Integer[] intArr : humidityToLocationMap) {
+                System.out.println(" - " + Arrays.toString(intArr));
+            }
         }
     }
 
@@ -87,5 +160,42 @@ class day5 {
 
     public static ArrayList<Integer[]> parseNumbers(ArrayList<String> inputStrings, int index) {
         // Parses the number maps following the given index.
+
+        // Create an array list of integer arrays to hold the map values:
+        ArrayList<Integer[]> mapValues = new ArrayList<>();
+
+        // For each input string following the input:
+        for (int i = index; i < inputStrings.size(); i++) {
+            // Assumption: There's always a blank line after the final map line.
+            if (inputStrings.get(i).length() <= 1) {
+                break;
+            } else {
+                // Split the input string:
+                String[] integerStrings = inputStrings.get(i).split(" ");
+
+                // Create an integer array to hold the output:
+                ArrayList<Integer> integerArrayList = new ArrayList<>();
+
+                // Parse each string into an integer:
+                for (String intString : integerStrings) {
+                    if (checkIntegerString(intString)) {
+                        integerArrayList.add(Integer.parseInt(intString));
+                    }
+                }
+
+                // Map values always come in 3:
+                if (integerArrayList.size() == 3) {
+                    // Convert the Array List to an array:
+                    Integer[] integerArray = new Integer[integerArrayList.size()];
+                    integerArray = integerArrayList.toArray(integerArray);
+
+                    // Add the integer array to the map value array list:
+                    mapValues.add(integerArray);
+                }
+            }
+        }
+
+        // Return the map value array list:
+        return mapValues;
     }
 }
